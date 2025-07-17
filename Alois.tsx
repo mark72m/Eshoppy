92be05c9bad07a8ddc600167180d340fef80dc99
markm <markmutisya72@gmail.com>
2025-07-17 10:08:22 -0700

    Alicios

---
+++ Alois.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Alois = (props: Props) => {
+  return (
+    <View>
+      <Text>Alois</Text>
+    </View>
+  )
+}
+
+export default Alois
