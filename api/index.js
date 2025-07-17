b8157fd08fa910ca538bc1198f5f24da3cd643d2
markm <markmutisya72@gmail.com>
2025-07-17 14:34:46 -0700

    Screens

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require ('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
@@ -13,3 +13,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const jwt = require('jsonwebtoken');
+
+mongoose.connect("mongodb+srv://mark72m:char2N'sLie@cluster0.gw7ljpr.mongodb.net/", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("Error Connecting to MongoDb", err);
+    });
+
+app.listen(port, () => {
+    console.log("Server is runninng on port 8000");
+});
