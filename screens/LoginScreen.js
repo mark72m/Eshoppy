ff8c9aff34b183ce11833862c20e8d7ab4e05f89
markm <markmutisya72@gmail.com>
2025-07-19 12:41:00 -0700

    Login Start

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, SafeAreaView, View, Image, KeyboardAvoidingView } from 'react-native'
 import React from 'react'
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'white', alignItems: 'center', justifyContent: 'center' }}>
+      <View>
+        <Image
+          style={{ width: 150, height: 100 }}
+          source={{
+            uri: "https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png"
+          }} />
+
+      </View>
+
+      <KeyboardAvoidingView>
+        <View>
+          <Text>Log In to Your Account</Text>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   )
 }
 
-export default LoginScreen
+export default LoginScreen;
 
 const styles = StyleSheet.create({})
